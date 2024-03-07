dccf11cf4a58999372c589b2ab262d85f9f5572a
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2024-03-07 12:27:14 +0530

    Template for the Website

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Database Connection Details
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'roomatedb');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+// Application Details
+define('APP_NAME', 'Roomate');
+define('BASE_URL', 'http://localhost/roomate/');
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>This is the main page</title>
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="/roomate/assets/css/style.css">
+
+	<script src="/roomate/assets/js/script.js" defer></script>
+</head>
+
+<body>
+
 <?php
-	echo "Hello World!";
+include 'templates/navbar.html';
+include 'templates/header.html';
 ?>
+
+	<div class="flex containerBlock">
+		<div class="flex flexCol container">
+
+		<?php
+		include 'templates/sample.html';
+		?>
+
+		</div>
+	</div>
+
+<?php
+include 'templates/footer.html';
+?>
+
+</body>
+
+</html>
