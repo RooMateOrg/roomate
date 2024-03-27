32d5fe815b137e2c5b41bb092445882c435229bc
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2024-03-27 09:41:01 +0530

    Finished Add Blog - Admin

---
+++ api/addblog.php
@@ -0,0 +1,20 @@
+<?php
+include '../config/dbconnection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$title = $_POST['blogName'];
+	$category = $_POST['blogCategory'];
+	$image = $_POST['blogImage'];
+	$description = $_POST['blogDescription'];
+	$url = $_POST['blogLink'];
+
+	$sql = "INSERT INTO blogs (title, category, image, description, url) VALUES ('$title', '$category', '$image', '$description', '$url')";
+
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+	$conn->close();
+}
